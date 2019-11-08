b9978371d9ccf5a28f8d36ab139bd5a54bba4e2f
kunalc <kunalc@QA020.bitsdev.in>
2019-11-08 17:10:53 +0530

    New Changes

---
+++ FiltaSymphony/src/main/java/com/filta/qa/operations/OperationsSubModule.java
@@ -0,0 +1,528 @@
+package com.filta.qa.operations;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+//import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.filta.qa.util.Global;
+import com.filta.qa.util.Login;
+import com.filta.qa.util.ReadExcelData;
+
+public class OperationsSubModule
+{
+	private WebDriver driver;
+	private Global globalObj;
+	private Properties prop;
+	private Login login;
+	private String fBStatus;
+	private boolean oPSAccess;
+	private ReadExcelData operations;
+	private ArrayList<String> array;
+
+	@FindBy(how = How.XPATH, using = ".//a[@class='container-close']")
+	private WebElement containerClose;
+
+	@FindBy(how = How.XPATH, using = ".//div[@id='ajaxHeader']//a[@id='grouptab_1']")
+	private WebElement franchisee;
+
+	@FindBy(how = How.XPATH, using = ".//div[@id='ajaxHeader']//a[@id='moduleTab_-1_Franchisees']")
+	private WebElement franchiseeClick;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='search_name_basic']")
+	private WebElement nameFranchiseeSearch;
+
+	@FindBy(how = How.XPATH, using = ".//input[@type='submit']")
+	private WebElement nameFranchiseeSearchClick;
+
+	@FindBy(how = How.XPATH, using = ".//table[@id='ListTable']//tbody//tr[3]//td[3]//a")
+	private WebElement franchiseeIDClick;
+
+	@FindBy(how = How.XPATH, using = ".//a[@id='tab1']")
+	private WebElement filtaBioTab;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='fb_status']")
+	private WebElement filtaBioStatus;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='ops_access']")
+	private WebElement oPSCheckbox;
+
+	@FindBy(how = How.XPATH, using = ".//a[@id='logout_link']")
+	private WebElement logout;
+
+	@FindBy(how = How.XPATH, using = ".//div[@id='ajaxHeader']//a[@id='grouptab_3']")
+	private WebElement operationTab;
+
+	@FindBy(how = How.XPATH, using = ".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li")
+	private WebElement operationList;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='edit_button']")
+	private WebElement editFranchisee;
+
+	@FindBy(how = How.XPATH, using = ".//div[@id='EditView_tabs']//li[2]//a[@id='tab1']")
+	private WebElement editFB;
+
+	@FindBy(how = How.XPATH, using = ".//select[@id='fb_status']")
+	private WebElement currentStatus;
+
+	@FindBy(how = How.XPATH, using = ".//table[@class='dcQuickEdit']//div[@class='action_buttons action-rows']//input[@id='SAVE_HEADER']")
+	private WebElement saveFranchisee;
+
+	@FindBy(how = How.XPATH, using = ".//select[@id='jac_region']")
+	private WebElement jacobsen;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='thousand']")
+	private WebElement thousand;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='fifteen_hundred']")
+	private WebElement fifteenHundred;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='two_thousand']")
+	private WebElement twoThousand;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='twenty_five_hundred']")
+	private WebElement twentyFiveHundred;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='no_limit']")
+	private WebElement noLimit;
+
+	@FindBy(how = How.XPATH, using = ".//button[@id='btn_franchisee_6k_name']")
+	private WebElement sixKFR;
+
+	@FindBy(how = How.XPATH, using = "/html/body/div[1]/table/tbody/tr[3]/td[1]/a")
+	private WebElement first6K;
+
+	@FindBy(how = How.XPATH, using = ".//button[@id='btn_fl_routes_fl_franchisee_name']")
+	private WebElement route;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='rate_paid']")
+	private WebElement ratePaid;
+
+	@FindBy(how = How.XPATH, using = ".//input[@id='filta_perc']")
+	private WebElement filtapercentage;
+
+
+	public OperationsSubModule(WebDriver driver)
+	{
+		globalObj = new Global();
+		prop = globalObj.readProperties();
+		this.driver = driver;
+		login = new Login(driver);
+		operations = new ReadExcelData(prop.getProperty("Path2"), "Operations");
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public void opsActivePercent()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			fBStatus = filtaBioStatus.getAttribute("value");
+			System.out.println(fBStatus);
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 0));
+		}
+		logout.click();
+	}
+
+
+	public void opsOnly()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Active Percent"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Inactive");
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+//		
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 1));
+		}
+		logout.click();
+	}
+
+
+	public void opsActive1KProgram()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Inactive"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Active 1k Program");
+				globalObj.select(jacobsen).selectByVisibleText("New York");
+				thousand.sendKeys("2");
+				fifteenHundred.sendKeys("3.2");
+				twoThousand.sendKeys("4.0");
+				twentyFiveHundred.sendKeys("5.1");
+				noLimit.sendKeys("6");
+				sixKFR.click();
+				array = new ArrayList<String>(driver.getWindowHandles());
+				driver.switchTo().window(array.get(1));
+				first6K.click();
+				driver.switchTo().window(array.get(0));
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 2));
+		}
+		logout.click();
+
+	}
+
+
+	public void opsActive6KProgram()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Active 1k Program"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Active 6k Program");
+				route.click();
+				array = new ArrayList<String>(driver.getWindowHandles());
+				driver.switchTo().window(array.get(1));
+				first6K.click();
+				driver.switchTo().window(array.get(0));
+				ratePaid.sendKeys("2.3");
+				filtapercentage.sendKeys("1.6");
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 3));
+		}
+		logout.click();
+	}
+
+
+	public void opsActiveCollector()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Active 6k Program"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Active Collector");
+				globalObj.select(jacobsen).selectByVisibleText("New York");
+				thousand.sendKeys("2");
+				fifteenHundred.sendKeys("3.2");
+				twoThousand.sendKeys("4.0");
+				twentyFiveHundred.sendKeys("5.1");
+				noLimit.sendKeys("6");
+				route.click();
+				array = new ArrayList<String>(driver.getWindowHandles());
+				driver.switchTo().window(array.get(1));
+				first6K.click();
+				driver.switchTo().window(array.get(0));
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 4));
+		}
+		logout.click();
+	}
+
+
+	public void opsSignedWaiting()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Active Collector"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Signed Waiting");
+				globalObj.select(jacobsen).selectByVisibleText("New York");
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 5));
+		}
+		logout.click();
+	}
+
+
+	public void opsWCIssues()
+	{
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("Signed Waiting"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("WC Issues");
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+		loginOwner();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(operationTab));
+		globalObj.action(driver).moveToElement(operationTab).build().perform();
+		List<WebElement> list = driver.findElements(By.xpath(
+				".//div[@id='ajaxHeader']//div[@class='menubar']//div[@id='moduleList']//ul//li[@class='last-li'][3]//li//a"));
+//		Iterator<WebElement> itr = list.iterator();
+//		while (itr.hasNext())
+//		{
+//			System.out.println(itr.next().getText());
+//		}
+		for (int i = 0; i < list.size(); i++)
+		{
+			assertEquals(list.get(i).getText(), operations.getCellData(i + 1, 6));
+		}
+		logout.click();
+		loginAdmin();
+		globalObj.action(driver).moveToElement(franchisee).build().perform();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeClick));
+		franchiseeClick.click();
+		nameFranchiseeSearch.clear();
+		nameFranchiseeSearch.sendKeys(prop.getProperty("Franchisee_Name"));
+		nameFranchiseeSearchClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(franchiseeIDClick));
+		franchiseeIDClick.click();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(oPSCheckbox));
+		oPSAccess = oPSCheckbox.isSelected();
+		globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+		if (oPSAccess)
+		{
+			filtaBioTab.click();
+			if (filtaBioStatus.getAttribute("value").equals("WC Issues"))
+			{
+				editFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(editFB));
+				filtaBioTab.click();
+				globalObj.select(currentStatus).selectByVisibleText("Active Percent");
+				ratePaid.sendKeys("2.6");
+				saveFranchisee.click();
+				globalObj.wait(driver).until(ExpectedConditions.visibilityOf(filtaBioTab));
+				fBStatus = filtaBioStatus.getAttribute("value");
+				System.out.println(fBStatus);
+			}
+		}
+		logout.click();
+	}
+
+
+	public void loginOwner()
+	{
+		login.credentials();
+		containerClose.click();
+	}
+
+
+	public void loginAdmin()
+	{
+		login.adminCredentials();
+	}
+
+
+	public void closeBrowser()
+	{
+		driver.close();
+	}
+}
---
+++ FiltaSymphony/src/test/java/com/filta/qa/operations/scheduledjobcalendar_test/OperationsSubModule_TestCase.java
@@ -0,0 +1,186 @@
+package com.filta.qa.operations.scheduledjobcalendar_test;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.filta.qa.operations.OperationsSubModule;
+import com.filta.qa.util.Global;
+
+/**
+ * @author kun24
+ * @implNote Operations Sub Module Test Case
+ */
+
+public class OperationsSubModule_TestCase
+{
+	private WebDriver driver;
+	private Global globalObj;
+	private OperationsSubModule Obj;
+	private ExtentReports extent;
+	private ExtentHtmlReporter htmlReporter;
+	private ExtentTest extentLogger;
+	private String screenshotPath;;
+	private String dateName;
+	private File scrFile;
+
+	public static Logger Log = Logger.getLogger("Operations Module Test Cases");
+	static
+	{
+		PropertyConfigurator.configure(".//Log4j.properties");
+	}
+
+
+	@BeforeClass
+	public void property()
+	{
+		globalObj = new Global();
+		driver = globalObj.driver();
+		// Prop = Global_Obj.readProperties();
+		Obj = new OperationsSubModule(driver);
+		htmlReporter = new ExtentHtmlReporter(
+				System.getProperty("user.dir") + "/Symphony_Reports/UserPage/Operations Submodule.html");
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("OS", "Windows 7 64 Bit");
+		extent.setSystemInfo("Browser", "Google Chrome");
+		extent.setSystemInfo("H", "Kunal Chavan");
+		extent.setSystemInfo("Testing:", "Functional Testing");
+		htmlReporter.config().setReportName("Operations Submodule Test Case");
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a'('zzz')'");
+		htmlReporter.loadXMLConfig("./extent-config.xml");
+	}
+
+
+	@Test(priority = 0)
+	public void test()
+	{
+		Log.info("Check Operations Sub Module With Active Percent FiltaBio");
+		extentLogger = extent.createTest("Test Case 0: Check Operations Sub Module With Active Percent FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsActivePercent();
+	}
+
+
+	@Test(priority = 1)
+	public void test1()
+	{
+		Log.info("Check Operations Sub Module Without FiltaBio");
+		extentLogger = extent.createTest("Test Case 1: Check Operations Sub Module Without FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsOnly();
+	}
+
+
+	@Test(priority = 2)
+	public void test2()
+	{
+		Log.info("Check Operations Sub Module With 1K FiltaBio");
+		extentLogger = extent.createTest("Test Case 2: Check Operations Sub Module With 1K FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsActive1KProgram();
+	}
+
+
+	@Test(priority = 3)
+	public void test3()
+	{
+		Log.info("Check Operations Sub Module With 6K FiltaBio");
+		extentLogger = extent.createTest("Test Case 3: Check Operations Sub Module With 6K FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsActive6KProgram();
+	}
+
+
+	@Test(priority = 4)
+	public void test4()
+	{
+		Log.info("Check Operations Sub Module With Active Collector FiltaBio");
+		extentLogger = extent.createTest("Test Case 4: Check Operations Sub Module With Active Collector FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsActiveCollector();
+	}
+
+
+	@Test(priority = 5)
+	public void test5()
+	{
+		Log.info("Check Operations Sub Module With Signed Waiting FiltaBio");
+		extentLogger = extent.createTest("Test Case 5: Check Operations Sub Module With Signed Waiting FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsSignedWaiting();
+	}
+
+
+	@Test(priority = 6)
+	public void test6()
+	{
+		Log.info("Check Operations Sub Module With WC Issues FiltaBio");
+		extentLogger = extent.createTest("Test Case 5: Check Operations Sub Module With WC Issues FiltaBio");
+		Obj.loginAdmin();
+		Obj.opsWCIssues();
+
+	}
+
+
+	@AfterMethod
+	public void takeScreenShotOnFailure(ITestResult testResult) throws IOException
+	{
+		if (testResult.getStatus() == ITestResult.FAILURE)
+		{
+			extentLogger.log(Status.FAIL,
+					MarkupHelper.createLabel(testResult.getName() + " - Test Case Failed", ExtentColor.RED));
+			extentLogger.log(Status.FAIL,
+					MarkupHelper.createLabel(testResult.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+			dateName = new SimpleDateFormat("dd MMMM yyyy zzzz").format(new Date());
+			scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			screenshotPath = System.getProperty("user.dir") + "/Operations_SubModule_TestCase/"
+					+ testResult.getName() + dateName + "_" + Arrays.toString(testResult.getParameters()) + ".png";
+			FileUtils.copyFile(scrFile, new File(screenshotPath));
+			extentLogger.fail(
+					"Test Case Failed Snapshot is below " + extentLogger.addScreenCaptureFromPath(screenshotPath));
+
+		}
+		else if (testResult.getStatus() == ITestResult.SKIP)
+		{
+			extentLogger.log(Status.SKIP,
+					MarkupHelper.createLabel(testResult.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
+		}
+		else if (testResult.getStatus() == ITestResult.SUCCESS)
+		{
+			extentLogger.log(Status.PASS,
+					MarkupHelper.createLabel(testResult.getName() + " Test Case PASSED", ExtentColor.GREEN));
+		}
+	}
+
+
+	@AfterClass
+	public void afterMethod()
+	{
+		Log.info("Operations Sub Module Test Case Ends Here");
+		extent.flush();
+		Obj.closeBrowser();
+	}
+}
